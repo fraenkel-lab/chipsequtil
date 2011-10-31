7ec3b9e7356d369dace8e831ad3a3f1e1686a333
Adam Labadorf <alabadorf@gmail.com>
2011-10-31 13:20:51 -0400

    added stacked barplot

--- scripts/plot_peak_loc_dist.py
+++ scripts/plot_peak_loc_dist.py
@@ -13,7 +13,7 @@ from csv import reader, writer
 from optparse import OptionParser
 from StringIO import StringIO
 
-from chipsequtil import MACSFile
+from chipsequtil import MACSFile, BEDFile
 
 
 usage = '%prog [options] <peaks fn> <gene list fn>'
@@ -21,7 +21,8 @@ desc = """Produce a pie chart of the locations of peaks in different bins
 (promoter, gene, exon, intron, etc.) and, optionally, save the different
 records to their own files for subsequent analysis.  Also produce a histogram
 of distance from feature values in mapping file. Peaks file is expected
-to be as output by MACS.  Gene list file is expected to be in the format as
+to be as output by MACS, or alternately as a BED file but then the -b plot
+is not available.  Gene list file is expected to be in the format as
 output by peaks_to_known_genes.py script."""
 parser = OptionParser(usage=usage,description=desc)
 parser.add_option('-b','--bar-fn',dest='bar_fn',default=None,help='filename for pvalue stacked bar chart')
@@ -31,6 +32,7 @@ parser.add_option('-f','--dist-fn',dest='dist_fn',default=None,help='filename fo
 parser.add_option('-s','--save',dest='save',action='store_true',help='write out files containing peaks for each category')
 parser.add_option('-d','--output-dir',dest='out_dir',default='.',help='output files created by --save option to this directory')
 parser.add_option('--no-plot',dest='no_plot',action='store_true',help='dont show (but save) the figure produced')
+parser.add_option('--peaks-format',dest='peak_fmt',type='choice',choices=['MACS','BED'],default='MACS',help='format of peaks file, either MACS or BED [default: MACS]')
 
 GENE_FIELD_NAMES = ['knowngene_id','gene_symbol']
 LOC_FIELD_NAMES = ['peak_loc','dist_from_feature','score','map_type','map_subtype']
@@ -48,7 +50,10 @@ if __name__ == '__main__' :
 
     peaks_fn, gene_fn = args
 
-    peaks_f = MACSFile(peaks_fn)
+    if opts.peak_fmt == 'BED' :
+        peaks_f = BEDFile(peaks_fn)
+    else :
+        peaks_f = MACSFile(peaks_fn)
 
     gene_reader = reader(open(gene_fn),delimiter='\t')
     gene_recs, macs_recs, loc_recs = [], [], []
@@ -78,7 +83,10 @@ if __name__ == '__main__' :
     if opts.save :
         def get_writer(fn) :
             fd = writer(open(fn,'w'),delimiter='\t')
-            fd.writerow(GENE_FIELD_NAMES+MACSFile.FIELD_NAMES+LOC_FIELD_NAMES)
+            header = MACSFile.FIELD_NAMES
+            if opts.peak_fmt == 'BED' :
+                header = BEDFile.FIELD_NAMES
+            fd.writerow(GENE_FIELD_NAMES+header+LOC_FIELD_NAMES)
             return fd
         fds = {}
 
@@ -89,7 +97,8 @@ if __name__ == '__main__' :
         key = loc['map_type']+'_%s'%loc['map_subtype'] if loc['map_subtype'] != '' else loc['map_type']
         loc_dist[key] += 1
         dist_to_features[key].append(int(loc['dist_from_feature']))
-        pvals[key].append(float(peak['-10*log10(pvalue)']))
+        if opts.peak_fmt == 'MACS' :
+            pvals[key].append(float(peak['-10*log10(pvalue)']))
 
         map_key = '%s:%d-%d'%(peak['chr'],peak['start'],peak['end'])
         unique_peaks[key].add(map_key)
@@ -117,8 +126,9 @@ if __name__ == '__main__' :
         map_key = '%s:%d-%d'%(peak['chr'],peak['start'],peak['end'])
         if map_key not in all_unique_peaks :
             unique_peaks['intergenic'].add(map_key)
-            pvals['intergenic'].append(peak['-10*log10(pvalue)'])
             intergenic.append(peak)
+            if opts.peak_fmt == 'MACS' :
+                pvals['intergenic'].append(peak['-10*log10(pvalue)'])
         num_peaks += 1
 
     num_int = len(intergenic)
@@ -181,34 +191,35 @@ if __name__ == '__main__' :
     mp.savefig(dist_fn)
     mp.clf()
 
-    fig = mp.figure(figsize=(4,4))
-    bar_ax = fig.add_axes((0.15,0.15,0.7,0.7))
-    pval_hists = {}
-    min_pval, max_pval = min([min(v) for v in pvals.values()]), max([max(v) for v in pvals.values()])
-    for key,pvals in pvals.items() :
-        vals, bins = np.histogram(pvals,range=(0,max_pval),bins=20)
-        lv = np.log10(vals)
-        lv[np.isneginf(lv)] = 0.1
-        pval_hists[key] = lv
+    if opts.peak_fmt == 'MACS' :
+        fig = mp.figure(figsize=(4,4))
+        bar_ax = fig.add_axes((0.15,0.15,0.7,0.7))
+        pval_hists = {}
+        min_pval, max_pval = min([min(v) for v in pvals.values()]), max([max(v) for v in pvals.values()])
+        for key,pvals in pvals.items() :
+            vals, bins = np.histogram(pvals,range=(0,max_pval),bins=20)
+            lv = np.log10(vals)
+            lv[np.isneginf(lv)] = 0.1
+            pval_hists[key] = lv
 
-    pval_items = [(k,pval_hists[k]) for k in bin_order]
-    bar_width = 0.85*(max_pval-min_pval)/(len(bins)-1)
-    print max_pval, min_pval, len(bins)
-    print 'bar_width:',bar_width
-    bars = []
-    b = bar_ax.bar(bins[:-1],pval_items[0][1],width=bar_width,color=colors[0])
-    bars.append(b)
-
-    sum_bottoms = pval_items[0][1]
-    for i, (key, pvals) in enumerate(pval_items[1:]) :
-        b = bar_ax.bar(bins[:-1],pvals,bottom=sum_bottoms,width=bar_width,color=colors[i+1])
+        pval_items = [(k,pval_hists[k]) for k in bin_order if pval_hists.has_key(k)]
+        bar_width = 0.85*(max_pval-min_pval)/(len(bins)-1)
+        print max_pval, min_pval, len(bins)
+        print 'bar_width:',bar_width
+        bars = []
+        b = bar_ax.bar(bins[:-1],pval_items[0][1],width=bar_width,color=colors[0])
         bars.append(b)
-        sum_bottoms += pvals
-    bar_ax.legend([b[0] for b in bars],[x[0] for x in pval_items])
-    bar_ax.axis((-10,max(bins),0,max(sum_bottoms)))
-    bar_ax.set_title('Peak map distribution by pvalue')
-    bar_ax.set_xlabel('-10*log10(pvalue)')
-    bar_ax.set_ylabel('relative log10(# peaks)')
 
-    pval_fn = fn_base+'_pval_bar.png' if opts.bar_fn is None else opts.bar_fn
-    mp.savefig(pval_fn)
+        sum_bottoms = pval_items[0][1]
+        for i, (key, pvals) in enumerate(pval_items[1:]) :
+            b = bar_ax.bar(bins[:-1],pvals,bottom=sum_bottoms,width=bar_width,color=colors[i+1])
+            bars.append(b)
+            sum_bottoms += pvals
+        bar_ax.legend([b[0] for b in bars],[x[0] for x in pval_items])
+        bar_ax.axis((-10,max(bins),0,max(sum_bottoms)))
+        bar_ax.set_title('Peak map distribution by pvalue')
+        bar_ax.set_xlabel('-10*log10(pvalue)')
+        bar_ax.set_ylabel('relative log10(# peaks)')
+
+        pval_fn = fn_base+'_pval_bar.png' if opts.bar_fn is None else opts.bar_fn
+        mp.savefig(pval_fn)
